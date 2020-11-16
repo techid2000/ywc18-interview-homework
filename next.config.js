98b1e11a59336dd25136a6887cc644d49aa6d0b0
techid2000 <techid2000@gmail.com>
2020-11-17 02:46:04 +0700

    🔧 add .next.config.js to supports less and sass

---
+++ next.config.js
@@ -0,0 +1,10 @@
+const withSass = require('@zeit/next-sass');
+const withLess = require('@zeit/next-less');
+
+module.exports = withSass(
+  withLess({
+    lessLoaderOptions: {
+      javascriptEnabled: true,
+    },
+  })
+);
