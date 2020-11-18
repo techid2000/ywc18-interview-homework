81cc79ae2e4b56717499bb54f52791b25ad7d9c9
techid2000 <techid2000@gmail.com>
2020-11-19 03:11:14 +0700

    ✨ add search constants file

---
+++ src/constants/searchConstants.js
@@ -0,0 +1,27 @@
+export const LOCATIONS = {
+  ALL: 'สถานที่ทั้งหมด',
+  NEAR_ME: 'พื้นที่ใกล้ฉัน',
+};
+
+export const CATEGORIES = {
+  ALL: 'ทั้งหมด',
+  FOOD_AND_BEVERAGE: 'ร้านอาหารและเครื่องดื่ม',
+  OTOP: 'ร้านค้า OTOP',
+  THONGFAH: 'ร้านธงฟ้า',
+  GENERAL: 'สินค้าทั่วไป',
+};
+
+export const PRICERANGE = {
+  ALL: 'ทั้งหมด',
+};
+
+export const SUBCATEGORIES = {
+  ALL: 'ทั้งหมด',
+};
+
+export const WORD = {
+  FOOD: 'อาหาร',
+  BEVERAGE: 'เครื่องดื่ม',
+  OTOP: 'OTOP',
+  THONGFAH: 'ธงฟ้า',
+};
