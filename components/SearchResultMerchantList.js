c7ec7ed249b2879f1530bed5b6bbfe577f1bdcb3
techid2000 <techid2000@gmail.com>
2020-11-18 18:26:51 +0700

    🤡 mock more merchant cards

--- components/SearchResultMerchantList.js
+++ components/SearchResultMerchantList.js
@@ -2,7 +2,14 @@ import SearchResultMerchantCard from './SearchResultMerchantCard';
 
 const SearchResultMerchantList = () => {
   return (
-    <div className="border w-full">
+    <div className="w-full grid gap-2 grid-cols-1">
+      <SearchResultMerchantCard />
+      <SearchResultMerchantCard />
+      <SearchResultMerchantCard />
+      <SearchResultMerchantCard />
+      <SearchResultMerchantCard />
+      <SearchResultMerchantCard />
+      <SearchResultMerchantCard />
       <SearchResultMerchantCard />
     </div>
   );
