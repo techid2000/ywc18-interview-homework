57822405800c0df0593a23f2295b06ee753fc688
techid2000 <techid2000@gmail.com>
2020-11-18 21:57:21 +0700

    ✨ add axios default config in app

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
+import axios from 'axios';
+
 import '../less/antd.less';
 import '../scss/tailwind-build.scss';
+
+axios.defaults.baseURL = process.env.API_PATH;
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
