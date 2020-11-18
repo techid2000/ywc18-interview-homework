48a52e805224f7fe18b8d79508a3408d082e1618
techid2000 <techid2000@gmail.com>
2020-11-18 21:58:11 +0700

    ✨ make result page ssr load search meta

--- pages/search/result.js
+++ pages/search/result.js
@@ -1,19 +1,36 @@
+import dynamic from 'next/dynamic';
+import axios from 'axios';
+
+import AppLayout from '../../layouts/AppLayout';
 import SearchFiltersSidebar from '../../components/SearchFiltersSidebar';
 import SearchResultMerchantList from '../../components/SearchResultMerchantList';
-import AppLayout from '../../layouts/AppLayout';
 
-const SearchResultPage = () => {
+import SearchMetaContext from '../../contexts/SearchMetaContext';
+
+const DynamicReactJson = dynamic(import('react-json-view'), { ssr: false });
+
+const SearchResultPage = ({ searchMeta }) => {
   return (
-    <AppLayout>
-      <div className="text-xl font-semibold">
-        ผลการค้นหา ร้านค้า OTOP ทั้งหมด
-      </div>
-      <div className="flex mt-12 items-start">
-        <SearchFiltersSidebar />
-        <SearchResultMerchantList />
-      </div>
-    </AppLayout>
+    <SearchMetaContext.Provider value={searchMeta}>
+      <AppLayout>
+        <div className="text-xl font-semibold">
+          ผลการค้นหา ร้านค้า OTOP ทั้งหมด
+        </div>
+        <div className="flex mt-12 items-start">
+          <SearchFiltersSidebar />
+          <SearchResultMerchantList />
+        </div>
+        {process.env.NODE_ENV === 'development' && (
+          <DynamicReactJson src={searchMeta} />
+        )}
+      </AppLayout>
+    </SearchMetaContext.Provider>
   );
 };
+
+export async function getServerSideProps() {
+  const res = await axios.get('/api/search/meta');
+  return { props: { searchMeta: res.data } };
+}
 
 export default SearchResultPage;
