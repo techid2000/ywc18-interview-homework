89c2fe0accb7f523c783c10ed152cb1cb20aab15
techid2000 <techid2000@gmail.com>
2020-11-19 20:16:08 +0700

    🚀 add assetPrefix

--- next.config.js
+++ next.config.js
@@ -2,9 +2,12 @@ const withSass = require('@zeit/next-sass');
 const withLess = require('@zeit/next-less');
 const withImages = require('next-images');
 
+const prod = process.env.NODE_ENV === 'PRODUCTION';
+
 module.exports = withImages(
   withSass(
     withLess({
+      assetPrefix: prod ? '/ywc18-interview-homework' : '',
       lessLoaderOptions: {
         javascriptEnabled: true,
       },
