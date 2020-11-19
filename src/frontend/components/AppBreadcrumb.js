f4fd26bc03a4732b6d1fca0354ec39023f17e77c
techid2000 <techid2000@gmail.com>
2020-11-19 07:48:37 +0700

    💄 change some styles color

--- src/frontend/components/AppBreadcrumb.js
+++ src/frontend/components/AppBreadcrumb.js
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 const AppBreadcrumb = () => {
   return (
-    <div className="flex w-full justify-center items-center bg-dark-blue-600">
+    <div className="flex w-full justify-center items-center bg-dark-blue-700">
       <div
         className="flex w-full items-center px-0 md:px-10 pl-4 py-3"
         style={{ maxWidth: '1280px' }}
