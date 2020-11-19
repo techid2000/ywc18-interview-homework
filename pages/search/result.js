26969196259381062a21c2b29d390e459a4fe128
techid2000 <techid2000@gmail.com>
2020-11-19 07:48:16 +0700

    ✨ make better search result title

--- pages/search/result.js
+++ pages/search/result.js
@@ -10,18 +10,34 @@ import useSearch from '../../src/frontend/hooks/useSearch';
 import { getSearchMeta } from '../../src/frontend/api/searchAPI';
 import SearchContext from '../../src/frontend/contexts/SearchContext';
 import SearchMetaContext from '../../src/frontend/contexts/SearchMetaContext';
+import { CATEGORIES } from '../../src/constants/searchConstants';
+import { isEmpty } from 'lodash';
 
 const DynamicReactJson = dynamic(import('react-json-view'), { ssr: false });
 
+const getSearchResultTitle = (categoryName, searchQuery) => {
+  if (categoryName === CATEGORIES.ALL && isEmpty(searchQuery)) {
+    return 'ผลการค้นหาทั้งหมด';
+  }
+  if (categoryName === CATEGORIES.ALL) {
+    return `ผลการค้นหา ${searchQuery} ทั้งหมด`;
+  }
+  if (isEmpty(searchQuery)) {
+    return `ผลการค้นหา ${categoryName} ทั้งหมด`;
+  }
+  return `ผลการค้นหา ${categoryName}, ${searchQuery} ทั้งหมด`;
+};
+
 const SearchResultPage = ({ searchMeta }) => {
   const searchController = useSearch();
-  const { searchResult } = searchController;
+  const { categoryName, searchQuery, searchResult } = searchController;
+
   return (
     <SearchMetaContext.Provider value={searchMeta}>
       <SearchContext.Provider value={searchController}>
         <AppLayout>
           <div className="text-xl font-semibold">
-            ผลการค้นหา ร้านค้า OTOP ทั้งหมด
+            {getSearchResultTitle(categoryName, searchQuery)}
           </div>
           <div className="flex mt-12 items-start">
             <SearchFiltersSidebar />
