278f369a3994561132820e5c385bdc7904840a43
techid2000 <techid2000@gmail.com>
2020-11-19 21:41:27 +0700

    🚀 update path

--- next.config.js
+++ next.config.js
@@ -7,8 +7,8 @@ const prod = process.env.NODE_ENV === 'PRODUCTION';
 module.exports = withImages(
   withSass(
     withLess({
-      assetPrefix: prod ? '/ywc18-interview-homework/' : '',
-      basePath: prod ? '/ywc18-interview-homework/' : '',
+      assetPrefix: '/ywc18-interview-homework/',
+      basePath: '/ywc18-interview-homework',
       lessLoaderOptions: {
         javascriptEnabled: true,
       },
