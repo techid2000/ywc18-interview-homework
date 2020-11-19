4a1756c649e72e76acc82b848a243512460377c8
techid2000 <techid2000@gmail.com>
2020-11-19 07:49:26 +0700

    💄enhance drawer title

--- src/frontend/components/SearchFiltersDrawer.js
+++ src/frontend/components/SearchFiltersDrawer.js
@@ -1,14 +1,24 @@
+import { LeftOutlined } from '@ant-design/icons';
 import { Button, Drawer } from 'antd';
 import SearchFilters from './SearchFilters';
 
 const SearchFiltersDrawer = ({ visible, setDrawerVisible }) => {
   return (
     <Drawer
+      headerStyle={{
+        padding: 0,
+      }}
       title={
-        <>
-          <Button onClick={() => setDrawerVisible(false)}>Close</Button>
-          Basic Drawer
-        </>
+        <div className="relative bg-dark-blue-700 h-16 flex items-center justify-center">
+          <Button
+            type="text"
+            shape="circle"
+            className="absolute left-0 md-2 px-4"
+            icon={<LeftOutlined className="text-2xl text-white" />}
+            onClick={() => setDrawerVisible(false)}
+          />
+          <span className="text-2xl text-white">กรอกผล</span>
+        </div>
       }
       placement="right"
       closable={false}
